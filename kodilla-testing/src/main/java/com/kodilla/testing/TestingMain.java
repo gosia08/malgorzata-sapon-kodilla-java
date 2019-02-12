da0e352812f1e00e34b7da4e926adef1aec825c5
gosia08 <sapon.gosia@gmail.com>
2019-02-12 15:35:57 +0100

    Modul 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,18 @@
 package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        Calculator calculator= new Calculator();
+
+       int addResult = calculator.addMethod(3,3);
+       int subResult = calculator.subMethod(7,2);
+        if(addResult == 6)
+            if(subResult==5){
+            System.out.println("Test OK");
+
+        }else{
+            System.out.println("calculator has error!");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,17 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    int a;
+    int b;
+
+    public int addMethod (int a, int b) {
+
+        return a+b;
+    }
+
+    public int subMethod (int a, int b) {
+        return a-b;
+    }
+
+}
