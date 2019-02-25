e78a183c7b73b0f6b1d384e9179fdb5afd37d5d2
gosia08 <sapon.gosia@gmail.com>
2019-02-25 13:03:51 +0100

    Modul 7.0

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,7 +1,12 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.lambda.Executor;
+import com.kodilla.stream.lambda.Processor;
+
 public class StreamMain {
     public static void main(String[] args){
-        System.out.println("Welcome to module 7 - Stream");
+        Processor processor = new Processor();
+        Executor codeToExecute = () -> System.out.println("This is an example text.");
+        processor.execute(codeToExecute);
     }
 }
