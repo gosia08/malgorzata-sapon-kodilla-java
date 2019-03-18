89454b67a08f807ef1bae0c06427e2132ed9140e
gosia08 <sapon.gosia@gmail.com>
2019-03-18 14:42:50 +0100

    Modul 10

--- kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/library/Library.java
@@ -7,11 +7,14 @@ import java.util.List;
 @Service
 public final class Library {
     private final List<String> books = new ArrayList<>();
-
     private LibraryDbController libraryDbController;
 
-    public Library(LibraryDbController libraryDbController) {
+    public Library(final LibraryDbController libraryDbController) {
         this.libraryDbController = libraryDbController;
+    }
+
+    public Library () {
+
     }
 
     public void saveToDb() {
